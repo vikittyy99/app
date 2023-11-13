f7ad032a998b0a5c46ae9aa172fd91b100fdcdbe
vikittyy99 <74673021+vikittyy99@users.noreply.github.com>
2023-11-13 14:12:26 +0200

    -

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,10 +1,22 @@
-import { Text } from 'react-native'
+import { Text, View, SafeAreaView, ActivityIndicator, RefreshControl } from 'react-native';
+import { Stack, useRouter, useGlobalSearchParams } from 'expo-router';
+import { useCallback, useState } from 'react';
+import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from '../../components';
+import { COLORS, icons, SIZES } from '../../constants';
+import useFetch from '../../hook/useFetch';
 
 const JobDetails = () => {
+    const params = useGlobalSearchParams();
+    const router = useRouter();
+    const { data, isLoading, error, refetch } = useFetch('job-details', {
+        job_id: params.id
+    })
+
     return (
-        <Text>
-            Job details
-        </Text>
+        <SafeAreaView styles={{ flex: 1, backgroundColor: COLORS.lightWhite }}
+        >
+            <Stack.Screen> </Stack.Screen>
+        </SafeAreaView>
 
     )
 }
